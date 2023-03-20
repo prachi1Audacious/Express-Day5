88e6d1512d547646ea4bd90446738c41453d7dd8
Prachi <prachi.patel@techiegent.com>
2023-03-20 18:37:21 +0530

    day5

--- Router/route.js
+++ Router/route.js
@@ -1,7 +1,9 @@
 const route = require("express").Router()
-const controller = require("../Controller/clr")
+//const controller = require("../Controller/clr")
+const controller = require("../Controller/cntrl")
 
 
-route.post("/demo",controller.demo);
+//route.post("/demo",controller.demo);
+route.post("/users",controller.users);
 
 module.exports = route;
--- app.js
+++ app.js
@@ -15,8 +15,36 @@ app.get("/",(req,res) => {
     res.status(200).send('my name is prachi');
 })
 
+const users= [];
+
+app.post("/register", (req, res) => {
+  const { name, email } = req.body;
+  if (!name || !email) {
+    return res.status(400).send("Name, Email, are required!");
+  }
+  const user = users.find((demo) => users.email === email);
+  if (user) {
+    return res.status(400).send("Email already exists!");
+    
+  }
+  const newUser = {
+    name,
+    email
+    
+  };
+  demo.push(newUser);
+  // log user data to console
+  console.log(`New user: ${JSON.stringify(newUser)}`);
+  return res.status(201).send("Signup Successful!");
+});
+
+
+module.exports ={users};
+
+
 
 //server port
 app.listen(port,() => {
     console.log("server is running");
 })
+
